c46dc159724874e0ede7b65ab72bd8b3847b7af8
growthsilva <96981573+growthsilva@users.noreply.github.com>
2022-01-24 19:46:39 -0300

    Commit teste

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Meu Primeiro Projeto em ReactJS</h1>
+      <p>Olá, bem vindo ao meu primeiro projeto</p>
+     
+    </div>
+  )
+}
+
+export default App;
